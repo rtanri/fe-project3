09a6b370e8f4facb0f5c00826013920e098bc087
Revin Tanri <tanrirevin@gmail.com>
2021-07-10 16:09:15 +0800

    install material ui and scss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { createTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+
+const custom_theme = createTheme({
+  palette: {
+    primary: {
+      main: "#111B47",
+    },
+    secondary: {
+      main: "#0044ff",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={custom_theme}>
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    </ThemeProvider>
   );
+}
+
+function topHeader() {
+  return <div className="headerBackground"></div>;
 }
 
 export default App;
